9c79a32fe6f80df8a5387361adbd10388c52d898
vladimir1284 <vladimir.rdguez@gmail.com>
2023-11-16 21:46:26 -0500

    gETTING RATES

--- updater.py
+++ updater.py
@@ -3,15 +3,33 @@ import phpserialize
 import json
 from dotenv import load_dotenv
 import os
+import http.client
 
-new_value = 200
+# Get the current informal rates
+conn = http.client.HTTPSConnection("exchange-rate.decubba.com")
+payload = ''
+headers = {
+    'User-Agent': 'Apidog/1.0.0 (https://apidog.com)'
+}
+conn.request("GET", "/api/informal/cup", payload, headers)
+res = conn.getresponse()
+data = res.read()
+data_array = json.loads(data)
+
+# Rates using dollar as base
+cup_rate = float(data_array['exchange_rate'][0]["mid"])
+mlc_rate = cup_rate/float(data_array['exchange_rate'][1]["mid"])
+eur_rate = cup_rate/float(data_array['exchange_rate'][2]["mid"])
+print(f'cup_rate: {cup_rate}')
+print(f'mlc_rate: {mlc_rate}')
+print(f'eur_rate: {eur_rate}')
 
 # Get secrets
 load_dotenv()
 username = os.getenv('DB_USER')
 password = os.getenv('DB_PASS')
 host = os.getenv('DB_URL')
-port = os.getenv('DB_PORT')
+port = int(os.getenv('DB_PORT'))
 database = os.getenv('DATABASE')
 prefix = os.getenv('TABLE_PREFIX')
 
@@ -39,7 +57,9 @@ print(php_array)
 # exit(0)
 
 php_array = phpserialize.loads(option_value.encode('utf-8'))
-php_array[b'CUP'][b'rate_custom'] = new_value
+php_array[b'COP'][b'rate_custom'] = cup_rate
+php_array[b'EUR'][b'rate_custom'] = eur_rate
+php_array[b'MDL'][b'rate_custom'] = mlc_rate
 new_option_value = phpserialize.dumps(php_array)
 
 # Update the value in the database
